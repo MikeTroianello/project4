75ad9756ba16ca01fe87137e09f26794853a5dbb
Mike <troianello@protonmail.com>
2019-07-15 14:05:46 -0400

    stay awake

--- .history/client/src/api_20190320185731.js
+++ .history/client/src/api_20190320185731.js
@@ -1,27 +1,20 @@
-import axios from 'axios';
-axios.defaults.withCredentials = true;
+import axios from 'axios'
+axios.defaults.withCredentials = true
+
 
 const service = axios.create({
-  baseURL:
-    process.env.NODE_ENV === 'production'
-      ? '/api'
-      : 'http://localhost:5000/api',
+  baseURL: process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:5000/api',
   withCredentials: true
-});
-
-var http = require('http');
-setInterval(function() {
-  http.get('http://<your app name>.herokuapp.com');
-}, 300000); // every 5 minutes (300000)
+})
 
 const errHandler = err => {
-  console.error(err);
+  console.error(err)
   if (err.response && err.response.data) {
-    console.error('API response', err.response.data);
-    throw err.response.data.message;
+    console.error("API response", err.response.data)
+    throw err.response.data.message
   }
-  throw err;
-};
+  throw err
+}
 
 export default {
   service: service,
@@ -29,13 +22,13 @@ export default {
   // This method is synchronous and returns true or false
   // To know if the user is connected, we just check if we have a value for localStorage.getItem('user')
   isLoggedIn() {
-    return localStorage.getItem('user') != null;
+    return localStorage.getItem('user') != null
   },
 
   // This method returns the user from the localStorage
   // Be careful, the value is the one when the user logged in for the last time
   getLocalStorageUser() {
-    return JSON.parse(localStorage.getItem('user'));
+    return JSON.parse(localStorage.getItem('user'))
   },
 
   // This method signs up and logs in the user
@@ -44,31 +37,33 @@ export default {
       .post('/signup', userInfo)
       .then(res => {
         // If we have localStorage.getItem('user') saved, the application will consider we are loggedin
-        localStorage.setItem('user', JSON.stringify(res.data));
-        return res.data;
+        localStorage.setItem('user', JSON.stringify(res.data))
+        return res.data
       })
-      .catch(errHandler);
+      .catch(errHandler)
   },
 
   login(username, password) {
     return service
       .post('/login', {
         username,
-        password
+        password,
       })
       .then(res => {
         // If we have localStorage.getItem('user') saved, the application will consider we are loggedin
-        localStorage.setItem('user', JSON.stringify(res.data));
-        return res.data;
+        localStorage.setItem('user', JSON.stringify(res.data))
+        return res.data
       })
-      .catch(errHandler);
+      .catch(errHandler)
   },
 
   logout() {
-    localStorage.removeItem('user');
-    return service.get('/logout');
-  }
+    localStorage.removeItem('user')
+    return service
+      .get('/logout')
+  },
 
   // This is an example on how to use this method in a different file
   // api.getCountries().then(countries => { /* ... */ })
-};
+ 
+}
